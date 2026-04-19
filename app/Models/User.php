a43b542ef47037cea72d2ed9b5cfb6c3daa3e719
Aoki <aokigaharayuurei@gmail.com>
2026-04-19 16:54:55 +0500

    админка

--- app/Models/User.php
+++ app/Models/User.php
@@ -22,6 +22,11 @@ class User extends Authenticatable
         'email',
         'password',
     ];
+    const ADMIN_ROLE = 'admin';
+
+    public function isAdmin(){
+        return $this->role === self::ADMIN_ROLE;
+    }
 
     /**
      * The attributes that should be hidden for serialization.
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -16,11 +16,6 @@
     </head>
     <body class="font-sans text-gray-900 antialiased">        
         <div class="flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-[#FFF]">
-            <div>
-                <a href="/">
-                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-                </a>
-            </div>
 
             <div class="w-full sm:max-w-md mt-28 px-6 py-4 bg-[#E7E9EF] shadow-md overflow-hidden sm:rounded-lg">
                 {{ $slot }}
